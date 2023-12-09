f3d9576101f15e713cfe60d8af2ba673d00b6896
Prathmesh Sutar <40636045+Prathm-s@users.noreply.github.com>
2023-12-09 13:50:44 +0530

    Update calc.py
    added doc string

--- calc.py
+++ calc.py
@@ -1,6 +1,16 @@
 class Calculator:
   def add_by_prathms(self, num_1, num_2):
+    """"Add num_1 and num_1
+    num_1 : first number
+    num_2 : Second number
+    return result of two numbers
+    """
     return num_1 + num_2
 
   def sub_by_prathms(self,num_1, num_2):
+    """"Substract num_1 and num_1
+    num_1 : first number
+    num_2 : Second number
+    return result of two numbers
+    """
     return num_1 - num_2
